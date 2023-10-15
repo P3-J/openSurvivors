065c9119b4372ac20322f9d4baaf042b9df4b863
P3-J <jako.keskkyla@gmail.com>
2023-10-15 23:43:11 +0300

    grid based movement & settings

--- assets/player/player.cs
+++ assets/player/player.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class player : CharacterBody2D
 {
@@ -7,15 +8,34 @@ public partial class player : CharacterBody2D
 	[Export]
 	public int Speed { get; set; } = 400;
 
-	public void GetInput()
+	public string currentDire = ""; 
+
+
+	public override void _Input(InputEvent @event)
 	{
-		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-		Velocity = inputDirection * Speed ;
+		if (@event.IsActionPressed("left"))
+		{
+			Position   += Vector2.Left * 128 / 2;
+			currentDire = "left";  
+		}
+		if (@event.IsActionPressed("right"))
+		{
+			Position += Vector2.Right * 128 / 2;
+			currentDire = "right";
+		}
+		if (@event.IsActionPressed("up"))
+		{
+			Position += Vector2.Up * 128 / 2;
+			currentDire = "up";
+		}
+		if (@event.IsActionReleased("down"))
+		{
+			Position += Vector2.Down * 128 / 2;
+			currentDire = "down";
+			Trace.WriteLine(currentDire);
+		}
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double delta) {
-			GetInput();
-			MoveAndSlide();
-	}
+	
 }
